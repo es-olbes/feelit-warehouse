7229c929e3eaec67128be38af5b34c475f2f56cd
Jonathan Olbes <olbes.es@gmail.com>
2024-11-14 07:20:37 +0100

    Server running on local OK

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-export const SERVER_PORT = 3000
+export const SERVER_URI_PORT = 3000
+
+export const SERVER_URI = 'http://localhost:'
 
 export const DB_URI_MONGO = 'mongodb://127.0.0.1:27017/feelitwarehouse'
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import app from './app.js'
-import {SERVER_PORT,  DB_URI_MONGO} from './config.js'
+import {SERVER_URI, SERVER_URI_PORT,  DB_URI_MONGO} from './config.js'
 import mongoose from 'mongoose'
 
 
 
-app.listen(SERVER_PORT)
-console.log('Server running on port:', SERVER_PORT)
+app.listen(SERVER_URI_PORT)
+console.log('Server running on: >>> ', SERVER_URI + SERVER_URI_PORT)
 
 const connectDB = async () => {
     try {
         await mongoose.connect(DB_URI_MONGO)
-        console.log('DB is connected to', DB_URI_MONGO)
+        console.log('DB is connected to successfully: >>> ', DB_URI_MONGO)
     } catch (error) {
         console.error(error)
     }
